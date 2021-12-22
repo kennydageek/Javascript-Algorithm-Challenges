872208de38be2932723da546b51e2937eda6f65b
Ajibade Kehinde David <ajibadekehinde1@gmail.com>
2021-12-22 01:58:01 +0000

    repeat String and deleting the even problem

---
+++ 08. String repeat/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+// Write a function that repeats a given string n times
+
+
+// Steps to follow
+
+*/
+
+const repeatStr = function (n, anyString) {
+  return `${anyString.repeat(n)};`;
+};
+
+console.log(repeatStr(5, "hello"));
