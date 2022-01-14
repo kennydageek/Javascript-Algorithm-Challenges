0af3f7363e29954a855d3df53691b88f09ec6f29
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-14 23:40:04 +0000

    Converting Boolean to String Done!

---
+++ 14. Converting Boolean to String/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*
+
+mplement a function which convert the given boolean value into its string representation.
+
+*/
+
+function booleanToString(b) {
+  return b === true ? "true" : "false";
+}
+
+console.log(booleanToString(5 < 3));
