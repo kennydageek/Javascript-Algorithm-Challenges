ec472ec6834b24abf21d861160049718936b4737
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-01-15 00:52:26 +0000

    String to Array done!

---
+++ 15. Convert a String to an Array/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/*
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+*/
+
+function stringToArray(string) {
+  return string.split(" ");
+}
