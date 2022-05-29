29738291686427695b03c888043c88205feac135
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-05-29 22:14:10 +0100

    Camelcase method

---
+++ 28. CamelCase Method/script.js
@@ -0,0 +1,27 @@
+/*
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+*/
+
+const testString = "camel case word"
+
+const camelCase = function (string) {
+    // 1) Split strings into 
+    const stringArray = string.split(" ");
+
+    // 2) loop over the array and capitalize the first letter of each word
+    let capitalize = stringArray.map(cur => {
+        const capitalize = cur[0].toUpperCase() + cur.slice(1);
+        return capitalize;
+    })
+
+    // 3) Join the element together
+    let capString = capitalize.join("");
+    return capString
+}
+
+console.log(camelCase(testString));
